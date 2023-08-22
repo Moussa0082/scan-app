de71694e8e0ac4dcdbd1db626c05cc0bbbed8820
Moussa0082 <bane8251@gmail.com>
2023-08-22 02:20:04 +0200

    00h 20min first

---
+++ src/main/java/com/example/presence/scannapp/ScannAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.presence.scannapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScannAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/presence/scannapp/ScannAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presence.scannapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScannAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
